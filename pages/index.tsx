6a712dae43d18f53a9390fb01d452bf6f55bb5c6
Luciano Morazzo <lucianomorazzofd@gmail.com>
2022-08-09 23:33:45 -0300

    static props added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,61 @@
-import { Button } from '@nextui-org/react';
-import type { NextPage } from 'next'
+import type { GetStaticProps, NextPage } from 'next'
+import Image from 'next/image';
 import { Layout } from '../components/layouts';
+import { pokemonListResponse, smallPokemon } from '../interfaces';
+
+interface Props {
+  pokemons: smallPokemon[];
+}
 
 
-const HomePage: NextPage = () => {
+const HomePage: NextPage<Props> = ({ pokemons }) => {
+
+ 
+  
   return (
-   <Layout>
-    <Button color='gradient'>
-      Hola Mundo
-    </Button>
+   <Layout title="Pokemons">
+      <ul>
+        {pokemons.map( pkm => 
+          <li>
+            <h1>{pkm.name}</h1>
+            <Image src={pkm.image} width={90} height={90}/>
+          </li>
+        )}
+      </ul>
+      
+    
    </Layout>
   )
 }
 
 export default HomePage;
+
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+
+
+
+  const url = 'https://pokeapi.co/api/v2/pokemon?limit=151';
+
+  const res = await fetch( url );
+
+  const data: pokemonListResponse = await res.json();
+
+  const pokemons: smallPokemon = data.results.map( (pkm, i) => ({
+    ...pkm,
+    id: i + 1,
+    image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i + 1}.png`
+  })
+  );
+
+ 
+  console.log(pokemons);
+  
+  
+
+  return {
+    props:{
+      pokemons
+    }
+  }
+}
